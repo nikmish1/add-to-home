58c472a37557459815949c3c761cbe0e525ca6cf
nikhil24 <nikhil@ipsator.com>
2019-03-03 23:21:44 +0530

    Updates

---
+++ precache-manifest.2eed636918d3f301035cddda8196a12f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nik2490/add-to-home/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5198c5bdb36c55c59776",
+    "url": "/nik2490/add-to-home/static/js/runtime~main.5198c5bd.js"
+  },
+  {
+    "revision": "dadbe868d9df346bf055",
+    "url": "/nik2490/add-to-home/static/js/main.dadbe868.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/nik2490/add-to-home/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "dadbe868d9df346bf055",
+    "url": "/nik2490/add-to-home/static/css/main.9abc45c6.chunk.css"
+  },
+  {
+    "revision": "18dc3f34ca15140f9c6ec54828528589",
+    "url": "/nik2490/add-to-home/index.html"
+  }
+];
