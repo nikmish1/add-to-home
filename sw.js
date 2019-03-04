259c67ce5509823928554f1bb165eb68bf205ac4
nikhil24 <nikhil@ipsator.com>
2019-03-05 00:54:17 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,17 +1,24 @@
-self.addEventListener( 'install', function () {
-    console.log('Installed service worker');
-  } );
+importScripts('/cache-polyfill.js');
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1'
+     ]);
+   })
+ );
+});
   
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(response) {
-          // Cache hit - return response
-          if (response) {
-            return response;
-          }
-          return fetch(event.request);
-        }
-      )
-    );
-  });
+self.addEventListener('fetch', function(event) {
+  console.log(event.request.url);
+ 
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+ });
