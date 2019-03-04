38ad25c2532eb3e8274da79785e1a7704fc22851
nikhil24 <nikhil@ipsator.com>
2019-03-05 01:18:04 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,22 +1,24 @@
 //importScripts('/cache-polyfill.js');
 
-self.addEventListener('install', function(e) {
- e.waitUntil(
-   caches.open('airhorner').then(function(cache) {
-     return cache.addAll([
-       '/add-to-home/index.html',
-       '/add-to-home/index.html?homescreen=1'
-     ]);
-   })
- );
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open('airhorner').then(function (cache) {
+      return cache.addAll([
+        '/add-to-home/index.html',
+        '/add-to-home/index.html?homescreen=1'
+      ]);
+    }).catch(function (err) {
+      console.log("Service Worker Failed to Register", err);
+    })
+  );
 });
-  
-self.addEventListener('fetch', function(event) {
+
+self.addEventListener('fetch', function (event) {
   console.log(event.request.url);
- 
+
   event.respondWith(
-    caches.match(event.request).then(function(response) {
+    caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
     })
   );
- });
+});
